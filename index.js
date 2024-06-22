d98fbd29cebf44dab39f6c9ca53d46a1fc1f5d70
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-06-22 20:39:56 +0530

    Implemented routing by using react-router-dom, also made a about us page and wrote some css for it. Css will be replaced by tailwind later.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './src/components/app';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './src/components/About';
+
+const reactRoute = createBrowserRouter([
+  {
+    path : '/',
+    element : <App/>
+  },
+  {
+    path : '/About',
+    element : <About/>
+  },
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<RouterProvider router={reactRoute} />);
